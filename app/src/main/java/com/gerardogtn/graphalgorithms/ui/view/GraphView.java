f5887aa3aa18925b8a57ab5e2ed4ba24462a9b0c
Gerardo Teruel <gerardogtn@gmail.com>
2015-11-01 14:09:40 -0600

    :rocket: Add simple GraphView

---
+++ app/src/main/java/com/gerardogtn/graphalgorithms/ui/view/GraphView.java
@@ -0,0 +1,23 @@
+package com.gerardogtn.graphalgorithms.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.gerardogtn.graphalgorithms.ui.fragment.GraphFragment;
+
+
+/**
+ * Created by gerardogtn on 11/1/15.
+ */
+public class GraphView extends View {
+
+
+    public GraphView(Context context) {
+        this(context, null);
+    }
+
+    public GraphView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
