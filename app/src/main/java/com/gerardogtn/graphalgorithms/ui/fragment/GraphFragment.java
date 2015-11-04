cf4afd3c44f1b3b23cbb91b26ae98122f50ac300
Gerardo Teruel <gerardogtn@gmail.com>
2015-11-04 13:09:43 -0600

    :rocket: Implement ShowDialogListener to communicate with graphview

--- app/src/main/java/com/gerardogtn/graphalgorithms/ui/fragment/GraphFragment.java
+++ app/src/main/java/com/gerardogtn/graphalgorithms/ui/fragment/GraphFragment.java
@@ -3,20 +3,27 @@ package com.gerardogtn.graphalgorithms.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gerardogtn.graphalgorithms.R;
 import com.gerardogtn.graphalgorithms.data.model.Node;
+import com.gerardogtn.graphalgorithms.ui.dialog.AddEdgeDialog;
+import com.gerardogtn.graphalgorithms.ui.dialog.AddNodeDialog;
 import com.gerardogtn.graphalgorithms.ui.view.GraphView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GraphFragment extends Fragment {
+public class GraphFragment extends Fragment implements GraphView.ShowDialogListener,
+        AddEdgeDialog.OnCreateEdgeListener{
 
-    GraphView graphView;
+    public static final String TAG_NODE_DIALOG = "NodeDialog";
+    public static final String TAG_EDGE_DIALOG = "EdgeDialog";
+
+    private GraphView graphView;
 
     public GraphFragment() {
 
@@ -30,12 +37,12 @@ public class GraphFragment extends Fragment {
         return fragment;
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_graph, container, false);
         graphView = (GraphView) v;
+        graphView.setEventListener(this);
         return v;
     }
 
@@ -43,4 +50,23 @@ public class GraphFragment extends Fragment {
         graphView.addNode(node);
     }
 
+    @Override
+    public void showNodeDialog() {
+        FragmentManager manager = getFragmentManager();
+        AddNodeDialog dialog = new AddNodeDialog();
+        dialog.show(manager, TAG_NODE_DIALOG);
+    }
+
+    @Override
+    public void showEdgeDialog() {
+        FragmentManager manager = getFragmentManager();
+        AddEdgeDialog dialog = new AddEdgeDialog();
+        dialog.setOnClickListener(this);
+        dialog.show(manager, TAG_EDGE_DIALOG);
+    }
+
+    @Override
+    public void onCreateEdge(int weight) {
+        graphView.addEdge(weight);
+    }
 }
