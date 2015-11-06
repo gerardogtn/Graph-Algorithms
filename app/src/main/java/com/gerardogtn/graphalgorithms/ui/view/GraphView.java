910490497ba1b90d131ee570b899e3e75638f480
Gerardo Teruel <gerardogtn@gmail.com>
2015-11-06 00:25:25 -0600

    :rocket::bowtie: Add support for threading the current algorithm in execution

--- app/src/main/java/com/gerardogtn/graphalgorithms/ui/view/GraphView.java
+++ app/src/main/java/com/gerardogtn/graphalgorithms/ui/view/GraphView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -19,26 +20,31 @@ import java.util.Iterator;
 /**
  * Created by gerardogtn on 11/1/15.
  */
-public class GraphView extends View {
+public class GraphView extends View implements Graph.OnGraphUpdateListener, Runnable {
 
     public static final String TAG = GraphView.class.getSimpleName();
 
     public static final int BACKGROUND_COLOR = 0xFFF8EFE0;
-    public static final int TEXT_COLOR = Color.BLACK;
+    public static final int NODE_TEXT_COLOR = Color.WHITE;
+    public static final int EDGE_TEXT_COLOR = 0xFFFF4081;
 
+    private int mIndex = 0;
     private boolean mWasMoved = false;
+    private boolean mIsDialogActive = false;
 
     private Paint mNodePaint;
+    private Paint mNodeVisitedPaint;
     private Paint mEdgePaint;
+    private Paint mEdgeActivePaint;
     private Paint mBackgroundPaint;
-    private Paint mTextPaint;
+    private Paint mNodeTextPaint;
+    private Paint mEdgeTextPaint;
 
     private Graph mGraph;
     private Node mCurrentNode;
     private Node mPreviousNode;
 
     private ShowDialogListener insertListener;
-
 
     public GraphView(Context context) {
         this(context, null);
@@ -47,6 +53,7 @@ public class GraphView extends View {
     public GraphView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mGraph = Graph.getInstance(false);
+        mGraph.setOnGraphUpdateListener(this);
         setUpPaints();
     }
 
@@ -61,18 +68,18 @@ public class GraphView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         PointF current = new PointF(event.getX(), event.getY());
 
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mWasMoved = false;
                 handleNewEdge(current);
                 break;
             case MotionEvent.ACTION_MOVE:
-                if (mCurrentNode != null){
+                if (mCurrentNode != null && !mIsDialogActive) {
                     moveNode(current);
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                if (mWasMoved){
+                if (mWasMoved) {
                     mCurrentNode = null;
                 }
                 break;
@@ -87,13 +94,13 @@ public class GraphView extends View {
     // REQUIRES: None.
     // MODIFIES: this.
     // EFFECTS: Adds node to mNodes and redraw.
-    public void addNode(Node node){
+    public void addNode(Node node) {
         mGraph.addNode(node);
         mCurrentNode = null;
         invalidate();
     }
 
-    public void setEventListener(ShowDialogListener edgeListener){
+    public void setEventListener(ShowDialogListener edgeListener) {
         this.insertListener = edgeListener;
     }
 
@@ -105,10 +112,24 @@ public class GraphView extends View {
     }
 
     private void setUpTextPaint() {
-        mTextPaint = new Paint();
-        mTextPaint.setColor(Color.BLACK);
-        mTextPaint.setStyle(Paint.Style.FILL);
-        mTextPaint.setTextSize(20);
+        setUpNodeTextPaint();
+        setUpEdgeTextPaint();
+    }
+
+    private void setUpNodeTextPaint() {
+        mNodeTextPaint = new Paint();
+        mNodeTextPaint.setColor(NODE_TEXT_COLOR);
+        mNodeTextPaint.setStyle(Paint.Style.FILL);
+        mNodeTextPaint.setTextSize(24);
+        mNodeTextPaint.setAntiAlias(true);
+    }
+
+    private void setUpEdgeTextPaint() {
+        mEdgeTextPaint = new Paint();
+        mEdgeTextPaint.setColor(EDGE_TEXT_COLOR);
+        mEdgeTextPaint.setStyle(Paint.Style.FILL);
+        mEdgeTextPaint.setTextSize(24);
+        mEdgeTextPaint.setAntiAlias(true);
     }
 
     private void setUpBackgroundPaint() {
@@ -118,31 +139,42 @@ public class GraphView extends View {
 
     private void setUpEdgePaint() {
         mEdgePaint = new Paint();
-        mEdgePaint.setColor(TEXT_COLOR);
+        mEdgePaint.setColor(Color.BLACK);
         mEdgePaint.setStyle(Paint.Style.STROKE);
         mEdgePaint.setStrokeWidth(5);
         mEdgePaint.setAntiAlias(true);
+
+        mEdgeActivePaint = new Paint();
+        mEdgeActivePaint.setColor(Color.RED);
+        mEdgeActivePaint.setStyle(Paint.Style.STROKE);
+        mEdgeActivePaint.setStrokeWidth(5);
+        mEdgeActivePaint.setAntiAlias(true);
     }
 
     private void setUpNodePaint() {
         mNodePaint = new Paint();
         mNodePaint.setColor(Node.COLOR);
         mNodePaint.setAntiAlias(true);
+        mNodeVisitedPaint = new Paint();
+        mNodeVisitedPaint.setColor(Node.COLOR_VISITED);
+        mNodeVisitedPaint.setAntiAlias(true);
     }
 
     // REQUIRES: None.
     // MODIFIES: None.
     // EFFECTS:  Draws nodes to screen from first added to last added.
     private void drawNodes(Canvas canvas) {
-        Iterator<Node> iterator = mGraph.getmNodes().descendingIterator();
-        while (iterator.hasNext()){
+        Iterator<Node> iterator = mGraph.getNodes().descendingIterator();
+        while (iterator.hasNext()) {
             Node node = iterator.next();
+            Paint paint = (node.wasVisited() ? mNodeVisitedPaint : mNodePaint);
 
             canvas.drawCircle(node.getX(),
                     node.getY(),
                     Node.RADIUS,
-                    mNodePaint);
-            canvas.drawText(String.valueOf(node.getId()), node.getX(), node.getY(), mTextPaint);
+                    paint);
+
+            canvas.drawText(String.valueOf(node.getId()), node.getX(), node.getY(), mNodeTextPaint);
         }
     }
 
@@ -150,10 +182,23 @@ public class GraphView extends View {
     // MODIFIES: None.
     // EFFECTS:  Draws edges.
     private void drawEdges(Canvas canvas) {
-        for (Edge edge : mGraph.getmEdges()){
+        for (Edge edge : mGraph.getEdges()){
             Node origin = edge.getOrigin();
             Node destination = edge.getDestination();
-            canvas.drawLine(origin.getX(), origin.getY(), destination.getX(), destination.getY(), mEdgePaint);
+
+            Paint paint = (edge.isActive() ? mEdgeActivePaint : mEdgePaint);
+            canvas.drawLine(origin.getX(), origin.getY(), destination.getX(), destination.getY(), paint);
+
+            int increment = 20;
+            if (destination.getX() < origin.getX() &&
+                    Math.abs(destination.getY() - origin.getY()) < Node.RADIUS_SQUARED ) {
+                increment = -20;
+            }
+
+            canvas.drawText(edge.getWeight() + ""
+                    , (origin.getX() + destination.getX()) / 2 + increment
+                    , (origin.getY() + destination.getY()) / 2 + increment
+                    , mEdgeTextPaint);
         }
     }
 
@@ -161,8 +206,12 @@ public class GraphView extends View {
     // MODIFIES: this.
     // EFFECTS:  Updates mCurrentNode position and redraws.
     private void moveNode(PointF current) {
-        mCurrentNode.updatePosition(current);
-        mWasMoved = true;
+        if (current.x > getWidth() || current.x < 0 || current.y > getHeight() || current.y < 0){
+
+        } else {
+            mCurrentNode.updatePosition(current);
+            mWasMoved = true;
+        }
         invalidate();
     }
 
@@ -182,22 +231,84 @@ public class GraphView extends View {
     private void setUpEdgeDialog() {
         if (mCurrentNode != null && mPreviousNode != null && !mCurrentNode.equals(mPreviousNode)) {
             insertListener.showEdgeDialog();
+            mIsDialogActive = true;
         }
     }
 
     // ASSUMES:  mCurrentNode is not null and mPreviousNode is not null.
     // MODIFIES: this.
     // EFFECTS:  Adds edge to GraphView and redraws.
-    public void addEdge(int weight){
+    public void addEdge(int weight) {
+        assert (mCurrentNode != null && mPreviousNode != null);
         mGraph.addEdge(mPreviousNode, mCurrentNode, weight);
         mCurrentNode = null;
         mPreviousNode = null;
+        invalidate();
+        mIsDialogActive = false;
+    }
+
+    @Override
+    public void redraw() {
+        this.post(new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+            }
+        });
+    }
+
+
+    // REQUIRES: None
+    // MODIFIES: this
+    // EFFECTS:  Initializes mIndex, and starts a new thread.
+    public void executeAlgorithm(int index){
+        mIndex = index;
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    // REQUIRES: None.
+    // MODIFIES: None.
+    // EFFECTS:  Executes algorithm if  0 <= mIndex <= 7, else throws IllegalArgumentException.
+    @Override
+    public void run() {
+
+        try {
+            if (mIndex == 0) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else if (mIndex == 1) {
+                mGraph.dfs();
+            } else if (mIndex == 2) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else if (mIndex == 3) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else if (mIndex == 4) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else if (mIndex == 5) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else if (mIndex == 6) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else if (mIndex == 7) {
+                Log.d(TAG, "Opcion: " + mIndex + " no implementada");
+            } else {
+                throw new IllegalArgumentException();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    public void clearVisited() {
+        mGraph.clearVisited();
         invalidate();
     }
 
     public interface ShowDialogListener {
         void showEdgeDialog();
+
         void showNodeDialog();
     }
+
 
 }
