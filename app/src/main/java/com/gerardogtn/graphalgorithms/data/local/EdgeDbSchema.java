fecd8bbb78446e6495cee0f2d78e9286b494ede6
Gerardo Teruel <gerardogtn@gmail.com>
2015-11-07 14:07:24 -0600

    :rocket: Add database schemas

---
+++ app/src/main/java/com/gerardogtn/graphalgorithms/data/local/EdgeDbSchema.java
@@ -0,0 +1,32 @@
+package com.gerardogtn.graphalgorithms.data.local;
+
+/**
+ * Created by gerardogtn on 11/6/15.
+ */
+public class EdgeDbSchema {
+    public static final String TABLE_NAME = "graphs";
+
+    public static final String COLUMN_ID               = "id";
+    public static final String COLUMN_ORIGIN_NODE      = "origin_node";
+    public static final String COLUMN_DESTINATION_NODE = "destination_node";
+    public static final String COLUMN_WEIGHT           = "weight";
+    public static final String COLUMN_IS_DIRECTED      = "is_directed";
+    public static final String COLUMN_IS_ACTIVE        = "is_active";
+
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "(" +
+                    COLUMN_ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_ORIGIN_NODE      + " INTEGER NOT NULL, " +
+                    COLUMN_DESTINATION_NODE + " INTEGER NOT NULL, " +
+                    COLUMN_WEIGHT           + " INTEGER NOT NULL, " +
+                    COLUMN_IS_DIRECTED      + " INTEGER NOT NULL, " +
+                    COLUMN_IS_ACTIVE        + " INTEGER NOT NULL, " +
+
+                    "FOREIGN KEY (" + COLUMN_ORIGIN_NODE + ") REFERENCES " +
+                    NodeDbSchema.TABLE_NAME + "(" + NodeDbSchema.COLUMN_ID + "), " +
+
+                    "FOREIGN KEY (" + COLUMN_DESTINATION_NODE + ") REFERENCES " +
+                    NodeDbSchema.TABLE_NAME + "(" + NodeDbSchema.COLUMN_ID + ")" +
+                    " ); ";
+
+}
---
+++ app/src/main/java/com/gerardogtn/graphalgorithms/data/local/NodeDbSchema.java
@@ -0,0 +1,27 @@
+package com.gerardogtn.graphalgorithms.data.local;
+
+/**
+ * Created by gerardogtn on 11/6/15.
+ */
+public class NodeDbSchema {
+
+    public static final String TABLE_NAME = "nodes";
+
+    public static final String COLUMN_ID         = "id";
+    public static final String COLUMN_DATA       = "data";
+    public static final String COLUMN_X_POSITION = "x";
+    public static final String COLUMN_Y_POSITION = "y";
+    public static final String COLUMN_IS_ACTIVE  = "active";
+    public static final String COLUMN_IS_VISITED = "visited";
+
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY ON CONFLICT REPLACE, " +
+                    COLUMN_DATA + " INTEGER NOT NULL, " +
+                    COLUMN_X_POSITION + " REAL NOT NULL, " +
+                    COLUMN_Y_POSITION + " REAL NOT NULL, " +
+                    COLUMN_IS_ACTIVE + " INTEGER NOT NULL, " +
+                    COLUMN_IS_VISITED + " INTEGER NOT NULL" +
+                    " ); ";
+
+}
