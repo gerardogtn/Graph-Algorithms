8218afd3ecf43173d286980352bfe5e4defb5620
Gerardo Teruel <gerardogtn@gmail.com>
2015-11-19 09:30:01 -0600

    :rocket: Add color constants

---
+++ app/src/main/java/com/gerardogtn/graphalgorithms/util/constant/Color.java
@@ -0,0 +1,17 @@
+package com.gerardogtn.graphalgorithms.util.constant;
+
+/**
+ * Created by gerardogtn on 11/19/15.
+ */
+public class Color {
+    public static final int ORANGE = 0xFFFF5722;
+    public static final int BLUE = 0xFF3F51B5;
+    public static final int DARK_BLUE = 0xFF303F9F;
+    public static final int RED = 0xFFF44336;
+    public static final int PINK = 0xFFFF4081;
+    public static final int WHITE = 0xFFFFFFFF;
+    public static final int BEIGE = 0xFFF8EFE0;
+    public static final int BLACK = 0xFF212121;
+    public static final int BLUE_GRAY = 0xFF607D8B;
+    public static final int PURPLE = 0xFF9C27B0;
+}
