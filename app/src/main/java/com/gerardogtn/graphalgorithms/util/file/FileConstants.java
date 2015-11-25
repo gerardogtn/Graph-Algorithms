8c4f5073f03f9968cd7b1e0f126beee873afefec
Gerardo Teruel <gerardogtn@gmail.com>
2015-11-24 19:08:42 -0600

    :lipstick: Create getPathToFile abstract method

--- app/src/main/java/com/gerardogtn/graphalgorithms/util/file/FileConstants.java
+++ app/src/main/java/com/gerardogtn/graphalgorithms/util/file/FileConstants.java
@@ -10,13 +10,15 @@ import java.io.File;
 public class FileConstants {
 
     public static final String GRAPH_IMAGE_NAME = "graph_image.jpeg";
-    public static final String GRAPH_IMAGE_PATH = Environment.getExternalStorageDirectory()
-            + File.separator
-            + GRAPH_IMAGE_NAME;
+    public static final String GRAPH_IMAGE_PATH = getPathToFile(GRAPH_IMAGE_NAME);
 
 
     public static final String GEXF_NAME = "graph.gexf";
-    public static final String GEXF_PATH = Environment.getExternalStorageDirectory()
-            + File.separator
-            + GEXF_NAME;
+    public static final String GEXF_PATH = getPathToFile(GEXF_NAME);
+
+    public static String getPathToFile(String fileName){
+        return Environment.getExternalStorageDirectory() + File.separator + fileName;
+    }
+
 }
+
